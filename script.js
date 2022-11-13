018bbbb8598dbad6c4680eaec13303e2912b9a5f
Patricia Fiol <patricia.fiol@outlook.com>
2022-11-13 08:22:31 +0100

    Add styling and basic functionality

---
+++ script.js
@@ -0,0 +1,12 @@
+const quoteContainer = document.getElementById('quote-generator')
+const quoteText = document.getElementById('quote')
+const authorText = document.getElementById('author')
+
+
+function newQuote() {
+  const Quote = localQuotes[Math.floor(Math.random() * localQuotes.length)]
+}
+
+
+
+newQuote()
